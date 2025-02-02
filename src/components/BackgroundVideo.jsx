775117b267d8ceb1ecb445df82e79e1dad2a5d4f
Andrey <andrii@3pace.dev>
2025-02-02 22:57:14 +0300

    init

---
+++ src/components/BackgroundVideo.jsx
@@ -0,0 +1,19 @@
+import React from "react"; // Подключите CSS для стилизации
+import BuddyGreene from "../assets/BuddyGreene.mp4";
+
+const BackgroundVideo = () => {
+  return (
+    <div className="video-container">
+      <video autoPlay loop muted>
+        <source src={BuddyGreene} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      {/* Контент, который будет поверх видео */}
+      <div className="content-overlay">
+        <h1>Your Content Here</h1>
+      </div>
+    </div>
+  );
+};
+
+export default BackgroundVideo;
---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import BackgroundVideo from "./BackgroundVideo";
+
+function Home() {
+  return (
+    <>
+      <div>Home</div>
+      <BackgroundVideo />
+    </>
+  );
+}
+
+export default Home;
