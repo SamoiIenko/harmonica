d8a1ab4600a2a2709ccb7429736d07a7c15b60da
Andrey <samoilenkoand@gmail.com>
2025-04-18 21:58:35 +0300

    fix bootstrap functionality for project

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import App from './app'
 import styles from './index.css'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 const rootElement = document.getElementById('root')
 
 const root = createRoot(rootElement)
