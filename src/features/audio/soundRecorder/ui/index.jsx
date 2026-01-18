8953009b18e3e486fe315214d4e78a7f319d5bbc
SamoiIenko <samoilenkoand@gmail.com>
2026-01-18 18:14:43 +0300

    buttons tests

--- src/features/audio/soundRecorder/ui/index.jsx
+++ src/features/audio/soundRecorder/ui/index.jsx
@@ -1,6 +1,17 @@
-import { memo } from 'react'
+import { memo, useEffect, useState } from 'react'
 
 const _SoundRecorder = () => {
+  const [activeNote, setActiveNote] = useState(null)
+
+  useEffect(() => {
+    if (activeNote) {
+      const timer = setTimeout(() => setActiveNote(null), 1000)
+      return () => clearTimeout(timer)
+    }
+  }, [activeNote])
+
+  const noteHandler = (notePosition) => setActiveNote(notePosition)
+
   const audioList = document.getElementById('audioList')
   const audioPlayer = document.getElementById('audioPlayer')
   let mediaRecorder
@@ -19,13 +30,15 @@ const _SoundRecorder = () => {
         }
 
         mediaRecorder.onstop = function () {
-          const audioBlob = new Blob(audioChunks, { type: 'audio/wav' })
+          const audioBlob = new Blob(audioChunks, { type: 'audio/webm' })
           const audioURL = URL.createObjectURL(audioBlob)
           const listItem = document.createElement('li')
           const audioLink = document.createElement('a')
           audioLink.href = audioURL
-          audioLink.download = 'audio.wav'
+          audioLink.download = 'audio.webm'
           audioLink.textContent = 'Download Audio'
+
+          console.log(audioLink)
           listItem.appendChild(audioLink)
           audioList.appendChild(listItem)
           audioPlayer.src = audioURL
@@ -58,6 +71,25 @@ const _SoundRecorder = () => {
       <button onClick={startRecordingButton}>Start recording</button>
       <button onClick={stopRecordingBuutton}>Stop recording</button>
       <ul id="audioList"></ul>
+      <div>
+        <button
+          style={{
+            backgroundColor: activeNote ? '#ffe26e' : 'green',
+          }}
+          onClick={(e) => noteHandler('one')}
+        >
+          1
+        </button>
+        <button onClick={(e) => noteHandler(activeNote, e.target)}>2</button>
+        <button onClick={(e) => noteHandler(activeNote, e.target)}>3</button>
+        <button onClick={(e) => noteHandler('four', e.target)}>4</button>
+        <button onClick={(e) => noteHandler('five', e.target)}>5</button>
+        <button onClick={(e) => noteHandler('six', e.target)}>6</button>
+        <button onClick={(e) => noteHandler('seven', e.target)}>7</button>
+        <button onClick={(e) => noteHandler('eight', e.target)}>8</button>
+        <button onClick={(e) => noteHandler('nine', e.target)}>9</button>
+        <button onClick={(e) => noteHandler('ten', e.target)}>10</button>
+      </div>
 
       <audio id="audioPlayer" controls></audio>
     </div>
